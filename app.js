42f13af37bbc96073ec031aea124bd9beea1b69f
gandhamathan <gandhasaroja@gmail.com>
2022-07-02 21:24:37 +0530

    testing

--- app.js
+++ app.js
@@ -2,15 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const pageRoute = require("./routes/pageRoutes");
-const cssRoute = require("./routes/cssRoutes");
-const imageRoute = require("./routes/imageRoutes");
-const databaseRoute = require("./routes/databaseRoutes");
 const app = express();
 app.use(bodyParser.json());
 app.use(morgan("combined"));
 app.use("/", pageRoute);
-app.use("/", cssRoute);
-app.use("/", imageRoute);
-app.use("/", databaseRoute);
+
 
 module.exports = app;
